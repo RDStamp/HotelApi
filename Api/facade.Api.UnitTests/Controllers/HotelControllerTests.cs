d5ddf04291d96d288b1df1ea7f8b578aa9a4a9a2
RDStamp <rstamp@hotmail.com>
2025-06-29 17:21:37 +0100

    Beginning of tests adding , added wire frame for tests.

---
+++ Api/facade.Api.UnitTests/Controllers/HotelControllerTests.cs
@@ -0,0 +1,86 @@
+﻿using facade.Api.Controllers;
+using facade.Api.UnitTests.Helpers;
+using facade.Core.Helpers;
+using facade.Core.Services.HotelService;
+using facade.Data.Data;
+using facade.Data.Entities.Public;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace facade.Api.UnitTests.Controllers;
+
+public class HotelControllerTests
+{
+
+    private Mock<BookingsDBContext> _hotelContextMock = null!;
+    private HotelService _hotelService = null!;
+    private HotelController _hotelController = null!;
+
+    public HotelControllerTests()
+    {
+        DbContextOptions<BookingsDBContext> dbContextOptions = new DbContextOptionsBuilder<BookingsDBContext>()
+            .Options;
+
+        _hotelContextMock = new Mock<BookingsDBContext>(dbContextOptions);
+        _hotelContextMock.Setup(x => x.Set<Hotel>()).Returns(FakeHotels.GetHotels());
+
+        _hotelService = new HotelService(_hotelContextMock.Object);
+
+        _hotelController = new HotelController(_hotelService);
+    }
+
+
+    [Fact]
+    public void GetHotelByName_WithNullName_ReturnsBadRequestStatus()
+    {
+        // Arrange
+
+        // Act
+        var controllerResponse = _hotelController.GetHotelByName(null);
+
+
+        //Assert
+        Assert.NotNull(controllerResponse);
+        Assert.IsType<BadRequestObjectResult>(controllerResponse.Result);
+        var result = (ObjectResult)controllerResponse.Result;
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+    }
+
+    [Fact]
+    public void GetHotelByName_WithEmptyName_ReturnsBadRequestStatus()
+    {
+        // Arrange
+
+        // Act
+        var controllerResponse = _hotelController.GetHotelByName(string.Empty);
+
+
+        //Assert
+        Assert.NotNull(controllerResponse);
+        Assert.IsType<BadRequestObjectResult>(controllerResponse.Result);
+        var result = (ObjectResult)controllerResponse.Result;
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+    }
+
+    [Fact]
+    public void GetHotelByName_WithValidNameName_ReturnsSuccessStatusHavingHotel()
+    {
+        // Arrange
+        var name = "Moq Test Hotel";
+
+        // Act
+        var controllerResponse = _hotelController.GetHotelByName(name);
+
+
+        //Assert
+        Assert.NotNull(controllerResponse);
+        Assert.IsType<Result<Hotel>>(controllerResponse.Result);
+        //var result = controllerResponse.Result;
+        //Assert.True(result.IsSuccess);
+        //Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        //Assert.Equal(name, result.Value.FullName);
+    }
+}
+
---
+++ Api/facade.Api.UnitTests/Helpers/FakeBookings.cs
@@ -0,0 +1,18 @@
+﻿using facade.Data.Entities.Public;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace facade.Api.UnitTests.Helpers;
+
+public class FakeBookings
+{
+    public static DbSet<Booking> GetBookings()
+    {
+        DbSet<Booking> bookings = new Mock<DbSet<Booking>>().Object;
+
+        
+
+        return bookings;
+
+    }
+}
